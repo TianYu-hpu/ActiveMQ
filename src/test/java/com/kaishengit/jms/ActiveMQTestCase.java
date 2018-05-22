c663fd8e9354ae383c7970bf3575f03f0bfe4ef8
tianyu <tianyu.hpu@gmail.com>
2018-05-22 19:31:28 +0800

    重试策略

--- src/test/java/com/kaishengit/jms/ActiveMQTestCase.java
+++ src/test/java/com/kaishengit/jms/ActiveMQTestCase.java
@@ -34,7 +34,6 @@ public class ActiveMQTestCase {
             //发送消息
             //messageProducer.send(textMessage);
             messageProducer.send(textMessage, DeliveryMode.PERSISTENT, i, 0);
-
         }
         //释放资源
         messageProducer.close();
@@ -74,6 +73,155 @@ public class ActiveMQTestCase {
 
                 } catch (JMSException e) {
                     e.printStackTrace();
+                }
+            }
+        });
+        System.in.read();
+        //释放资源
+        messageConsumer.close();
+        session.close();
+        connection.close();
+    }
+
+    /**
+     * 触发重试机制 rollback
+     * @throws JMSException
+     * @throws IOException
+     */
+    @Test
+    public void consumerMessageFromQueue1() throws JMSException, IOException {
+        //创建connectionFactory
+        String brokerUrl = "tcp://localhost:61616";
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+        //创建连接
+        Connection connection = connectionFactory.createConnection();
+        //开启连接
+        connection.start();
+        //创建回话
+        //客户端手动签收
+        //Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        final Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+        //目的地
+        Destination destination = session.createQueue("queue");
+        //创建消费者
+        MessageConsumer messageConsumer = session.createConsumer(destination);
+        //消费消息,如果这个消息队列里面有新的消息则会执行onMessage方法
+        messageConsumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    String text = textMessage.getText();
+                    if("Hello, MQ8".equals(text)) {
+                        throw new JMSException("故意抛出的异常");
+                    }
+                    //客户端手动签收
+                    //textMessage.acknowledge();
+                    System.out.println(textMessage.getText());
+                    session.commit();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                    try {
+                        session.rollback();
+                    } catch (JMSException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+        System.in.read();
+        //释放资源
+        messageConsumer.close();
+        session.close();
+        connection.close();
+    }
+
+    /**
+     * 触发重试机制 recover
+     * @throws JMSException
+     * @throws IOException
+     */
+    @Test
+    public void consumerMessageFromQueue2() throws JMSException, IOException {
+        //创建connectionFactory
+        String brokerUrl = "tcp://localhost:61616";
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+        //创建连接
+        Connection connection = connectionFactory.createConnection();
+        //开启连接
+        connection.start();
+        //创建回话
+        //客户端手动签收
+        final Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        //目的地
+        Destination destination = session.createQueue("queue");
+        //创建消费者
+        MessageConsumer messageConsumer = session.createConsumer(destination);
+        //消费消息,如果这个消息队列里面有新的消息则会执行onMessage方法
+        messageConsumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    String text = textMessage.getText();
+                    if("Hello, MQ8".equals(text)) {
+                        throw new JMSException("故意抛出的异常");
+                    }
+                    //客户端手动签收
+                    //textMessage.acknowledge();
+                    System.out.println(textMessage.getText());
+                    textMessage.acknowledge();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                    try {
+                        session.recover();
+                    } catch (JMSException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+        System.in.read();
+        //释放资源
+        messageConsumer.close();
+        session.close();
+        connection.close();
+    }
+
+    /**
+     * 触发重试机制 no catch
+     * @throws JMSException
+     * @throws IOException
+     */
+    @Test
+    public void consumerMessageFromQueue3() throws JMSException, IOException {
+        //创建connectionFactory
+        String brokerUrl = "tcp://localhost:61616";
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+        //创建连接
+        Connection connection = connectionFactory.createConnection();
+        //开启连接
+        connection.start();
+        //创建回话
+        //客户端手动签收
+        final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //目的地
+        Destination destination = session.createQueue("queue");
+        //创建消费者
+        MessageConsumer messageConsumer = session.createConsumer(destination);
+        //消费消息,如果这个消息队列里面有新的消息则会执行onMessage方法
+        messageConsumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    String text = textMessage.getText();
+                    if("Hello, MQ8".equals(text)) {
+                        throw new JMSException("故意抛出的异常");
+                    }
+                    System.out.println(textMessage.getText());
+                } catch (JMSException e) {
+                    throw new RuntimeException("运行时异常");
                 }
             }
         });
